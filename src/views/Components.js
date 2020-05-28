d5686c0e63c7ab6672834e01d93c656133f80f91
przemskydev <przemekgithub@o2.pl>
2020-05-28 22:54:25 +0200

    add dropdown

--- src/views/Components.js
+++ src/views/Components.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
+const items = [
+  { value: 'apple' },
+  { value: 'pear' },
+  { value: 'orange' },
+  { value: 'grape' },
+  { value: 'banana' },
+]
+
 const Components = () => {
-  return(
-    <h1 className="title is-3 is-spaced">Components</h1>
+  return (
+    <div>
+      <h1 className="title is-3 is-spaced">Components #renderprops #downshift 🏎</h1>
+
+      <div className="dropdown is-active">
+        <input className="input" type="text" placeholder="Text input" />
+        <div className="dropdown-menu">
+          <div className="dropdown-content">
+            {
+              items.map(({ value }) => (
+                <button key={value} className="button is-white dropdown-item">{value}</button>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
