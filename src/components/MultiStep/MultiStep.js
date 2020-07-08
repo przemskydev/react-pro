8ccf4a6ade6c4eb15c6ef641f82437902e7f858a
przemskydev <przemekgithub@o2.pl>
2020-07-08 14:52:27 +0200

    useContext

--- src/components/MultiStep/MultiStep.js
+++ src/components/MultiStep/MultiStep.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
-const Page = ({ children }) => {
-  return (
-    <div>
-      {children}
-    </div>
-  )
+const WrapperContext = React.createContext({
+  currentPage: 1,
+  changePage: () => { }
+});
+
+const Page = ({ children, pageId }) => {
+  const context = useContext(WrapperContext)
+
+  return context.currentPage === pageId ? children : null
 }
 
 const Controls = () => {
+  const context = useContext(WrapperContext)
+
   return (
     <div>
-      <button>Prev</button>
-      <button>Next</button>
+      <button className="button is-primary" onClick={() => context.changePage(context.currentPage - 1)}>Prev</button>
+      <button className="button is-primary" onClick={() => context.changePage(context.currentPage + 1)}>Next</button>
       {/* <button>Submit</button> */}
     </div>
   )
 }
 
 const Wrapper = ({ children }) => {
+  const [currentPage, setChangePage] = useState(1);
+
+  const changePage = pageIndex => {
+    setChangePage(pageIndex)
+  }
+
   return (
     <div>
-      {children}
+      <WrapperContext.Provider
+        value={{
+          currentPage,
+          changePage
+        }}>
+        {children}
+      </WrapperContext.Provider>
     </div>
   )
 }
--- src/views/Compound.js
+++ src/views/Compound.js
@@ -7,10 +7,30 @@ const Compound = () => (
   <div>
     <h1>Hello World!</h1>
     <MultiStep.Wrapper>
-      <MultiStep.Page>Page #1</MultiStep.Page>
-      <MultiStep.Page>Page #2</MultiStep.Page>
-      <MultiStep.Page>Page #3</MultiStep.Page>
+
+      <MultiStep.Page pageId={1}>
+        <div className="box">
+          <p>What's your name</p>
+          <input class="input" type="text" placeholder="First name"/>
+        </div>
+      </MultiStep.Page>
+
+      <MultiStep.Page pageId={2}>
+        <div className="box">
+          <p>Last name?</p>
+          <input class="input" type="text" placeholder="Last name"/>
+        </div>
+      </MultiStep.Page>
+
+      <MultiStep.Page pageId={3}>
+        <div className="box">
+          <p>Give your email</p>
+          <input class="input" type="email" placeholder="email"/>
+        </div>
+      </MultiStep.Page>
+
       <MultiStep.Controls />
+
     </MultiStep.Wrapper>
   </div>
 )
